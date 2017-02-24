3a43635fef2ba19db29adbc17373cc7d71c7d515
Vicente Neto <sneto.vicente@gmail.com>
2017-02-23 23:46:37 -0300

    Replacing simple positional formatting by the format function

--- result.py
+++ result.py
@@ -2,4 +2,4 @@ import pynettool
 
 with pynettool.NetworkElement('171.0.2.45') as ne:
     for route in ne.routing_table:
-        print '%15s -> %s' % (route.name, route.ip_addr)
+        print '{:>15} -> {}'.format(route.name, route.ip_addr)
