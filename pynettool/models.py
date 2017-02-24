168fbdff8f5f4d9507baf85b773b6a9525831f0d
Vicente Neto <sneto.vicente@gmail.com>
2017-02-23 23:19:41 -0300

    Creating RoutingTable class wrapper

--- pynettool/models.py
+++ pynettool/models.py
@@ -1,3 +1,5 @@
+import logging
+
 import jnettool.tools.elements.NetworkElement
 
 
@@ -11,3 +13,17 @@ class NetworkElement(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._ne.cleanup('commit')
         self._ne.disconnect()
+
+    @property
+    def routing_table(self):
+        try:
+            return RoutingTable(self._ne.getRoutingTable())
+        except jnettool.tools.elements.MissingVar:
+            logging.exception('No routing table found')
+            self._ne.cleanup('rollback')
+            return RoutingTable()
+
+
+class RoutingTable(object):
+    def __init__(self, routing_table=None):
+        self._rt = routing_table
