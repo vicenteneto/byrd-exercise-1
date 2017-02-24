c11c45a8fe97a352a1d75f6a6b19b835a278ee05
Vicente Neto <sneto.vicente@gmail.com>
2017-02-23 22:03:04 -0300

    Creating pynettool module
    * Initializing logger with NullHandler.

---
+++ pynettool/__init__.py
@@ -0,0 +1,20 @@
+"""
+PyNetTool library
+~~~~~~~~~~~~~~~~~
+
+PyNetTool is a Python wrapper for the jnettool library.
+"""
+
+import logging
+
+__title__ = 'pynettool'
+__author__ = 'Vicente Neto'
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
